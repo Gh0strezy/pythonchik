1327aaf336c73ae136ff630ddf4af8dc4bd187f2
Gh0strezy <superme2006@mail.ru>
2025-03-04 12:02:02 +0500

    Otchet

--- lab03/zov.py
+++ lab03/zov.py
@@ -1,31 +1,30 @@
-def findListRecursion(list1, numb):
+def find_list_recursion(list1, numb):
    if list1 == numb:
       return 0
    elif isinstance(list1, list):
       for index, number in enumerate(list1):
-         result = findListRecursion(number, numb)
+         result = find_list_recursion(number, numb)
          if result is not None:
             return index + result
    return None
 
-def findListWithoutRecursion2(list1, numb):
-   stack = [(list1, [])]  # Начальный стек: корневой список с глубиной 0
+def find_list_without_recursion2(list1, numb):
+   stack = [(list1, [])]
    while stack:
       current, depth = stack.pop()
       if current == numb:
          return sum(depth)
       if isinstance(current, list):
          for index, item in enumerate(current):
-               stack.append((item, depth + [index]))  # Увеличиваем глубину для каждого элемента
-         
-   return None # Если элемент не найден
-def findNumberWithoutRecursion(u, v, k):
+               stack.append((item, depth + [index]))
+   return None
+def find_number_without_recursion(u, v, k):
    a1, b1 = u, v
    for x in range(2, k+1):
       a1, b1 = 2 * b1 + a1, 2 * b1 * b1 + b1
    return a1, b1
 
-def findNumberWithRecursion(u, v, k):
+def find_number_with_recursion(u, v, k):
    def recursion(a1, b1, current_k):
       if current_k == k:
          return a1, b1
@@ -36,11 +35,12 @@ def findNumberWithRecursion(u, v, k):
    return recursion(u, v, 1)
 
 if __name__ == "__main__":
-   print(findListRecursion([1, 2, [3, 4, [5, [6, []]]]], 5))
-   print(findListRecursion([1, 2, [3, 4, [5, [6, []]]]], 'spam'))  
+   print(find_list_recursion([1, 2, [3, 4, [5, [6, []]]]], 4))
+   print(find_list_recursion([1, 2, [3, 4, [5, [6, []]]]], 'spam'))  
 
-   print(findListWithoutRecursion2([1, 2, [3, 4, [5, [6, []]]]], 5))
-   print(findListWithoutRecursion2([1, 2, [3, 4, [5, [6, []]]]], 'zov'))
+   print(find_list_without_recursion2([1, 2, [3, 4, [5, [6, []]]]], 4))
+   print(find_list_without_recursion2([1, 2, [3, 4, [5, [6, []]]]], 'zov'))
 
-   print(findNumberWithoutRecursion(1, 1, 3))
-   print(findNumberWithRecursion(1, 1, 3))
+   print(find_number_without_recursion(1, 1, 3))
+   print(find_number_with_recursion(1, 1, 3))
+   
